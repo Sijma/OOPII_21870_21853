b2af81611aea72fc3421f76e30dd045ea22a80ce
Sijma <39009079+Sijma@users.noreply.github.com>
2020-03-08 12:58:25 +0200

    Create Business.java

---
+++ Business.java
@@ -0,0 +1,52 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import weather.OpenWeatherMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author it21870
+ */
+public class Business extends Traveler
+{
+    private double lat,lon;
+    public static int MaxDistance = 20036;
+    private void SourceCityCoords(String SourceCityName) throws MalformedURLException, IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        OpenWeatherMap weather_obj = mapper.readValue(new URL("http://api.openweathermap.org/data/2.5/weather?q="+SourceCityName+"&APPID="+OpenData.appid+""), OpenWeatherMap.class);
+	lat = weather_obj.getCoord().getLat();
+        lon = weather_obj.getCoord().getLon();
+    }
+    
+    private double distance(double lat1, double lon1, double lat2, double lon2) 
+    {
+        lat1 = lat;
+        lon1 = lon;
+	if ((lat1 == lat2) && (lon1 == lon2)) 
+            {
+                return 0;
+            }
+       else 
+            {
+                double theta = lon1 - lon2;
+                double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+                dist = Math.acos(dist);
+                dist = Math.toDegrees(dist);
+                dist = dist * 60 * 1.1515 * 1.609344;
+                return (dist);
+            }
+    }
+    @Override
+    public double Similarity(City c)
+    {
+        return 1-(distance(lat,lon,c.lat,c.lon)/ MaxDistance);
+    }
+}
